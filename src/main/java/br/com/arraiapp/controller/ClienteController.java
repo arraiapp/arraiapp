2e1797e6db33b09fabacdd8452064b65dd3c7eec
RobertoWillian <awcostasilva@gmail.com>
2024-04-06 14:18:38 -0300

    Adicionado CrossOrigin no controller

--- src/main/java/br/com/arraiapp/controller/ClienteController.java
+++ src/main/java/br/com/arraiapp/controller/ClienteController.java
@@ -3,15 +3,13 @@ package br.com.arraiapp.controller;
 import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import br.com.arraiapp.model.Cliente;
 import br.com.arraiapp.service.ClienteService;
 
 @RestController
+@CrossOrigin (origins = "http://localhost:4200")
 public class ClienteController {
 
 
