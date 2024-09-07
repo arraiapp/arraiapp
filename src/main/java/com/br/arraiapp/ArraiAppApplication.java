f2d6880031955364c76cffd0ff98fa8db8ae89c0
RobertoWillian <awcostasilva@gmail.com>
2024-09-07 11:42:10 -0300

    Creating new spring project

---
+++ src/main/java/com/br/arraiapp/ArraiAppApplication.java
@@ -0,0 +1,13 @@
+package com.br.arraiapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ArraiAppApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ArraiAppApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/br/arraiapp/ArraiAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.br.arraiapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ArraiAppApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
