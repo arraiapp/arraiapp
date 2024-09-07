8cb8c79e4459b327b680a4369452af8113168903
RobertoWillian <awcostasilva@gmail.com>
2024-09-07 12:59:43 -0300

    [ADD] Created Stallholder and ticket classes

---
+++ src/main/java/com/br/arraiapp/domain/entity/Stallholder.java
@@ -0,0 +1,19 @@
+package com.br.arraiapp.domain.entity;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import com.br.arraiapp.domain.entity.User;
+
+@Data
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+@Table(name = "stallholder")
+public class Stallholder extends User {
+
+    private Boolean Admin;
+
+}
---
+++ src/main/java/com/br/arraiapp/domain/entity/Ticket.java
@@ -0,0 +1,29 @@
+package com.br.arraiapp.domain.entity;
+
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.math.BigDecimal;
+
+@NoArgsConstructor
+@AllArgsConstructor
+@Data
+@Entity
+@Table(name = "ticket")
+public class Ticket {
+
+    @Id
+    @GeneratedValue(strategy =  GenerationType.AUTO)
+    @Column(name="id")
+    private int id;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column( name = "value")
+    private BigDecimal value;
+
+}
